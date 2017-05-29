61fa798b75b23dc10edcee4670f4f1b9be311aad
CrownClown00 <rossca.thompson@gmail.com>
2017-05-29 10:22:06 -0400

    Inital Commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+})
+
+app.set('view engine','hbs')
+app.use(express.static(__dirname+'/public'));
+
+app.use((req,res,next)=>{
+    res.render('matienence.hbs'{
+      pageTitle:'Matienence Page'
+    })
+})
+
+app.get('/', (request,response)=>{
+  response.render('home.hbs',{
+    pageTitle:'Home page',
+    welcomeMessage:'Hey folks',
+
+  });
+});
+
+app.get('/about',(request,response)=>{
+  response.render('about.hbs',{
+    pageTitle:'About Page',
+
+  });
+})
+
+ app.get('/bad',(request,response)=>{
+   response.send({
+     error:"404 page"
+   })
+ })
+
+ app.listen(3000);
